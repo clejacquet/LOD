afdaf98715c714fcb24ffb5ba4406f6c54f3905f
clejacquet <clem.jacquet@gmail.com>
2015-04-20 17:34:11 +0900

    Add of a first app class

---
+++ App/src/main/java/jp/kde/lod/jacquet/app/App.java
@@ -0,0 +1,10 @@
+package jp.kde.lod.jacquet.app;
+
+/**
+ * Created by Clement on 20/04/2015.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
